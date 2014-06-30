139e072da0b79663f9ba5ba9bb8a213926498fb8
annaneo <aneovesky@gmail.com>
2014-06-30 22:32:25 +0200

    - new view controller for information

---
+++ VAUAboutListViewController.m
@@ -0,0 +1,49 @@
+//
+//  VAUAboutListViewController.m
+//  VerbannteAutoren
+//
+//  Created by anna on 30.06.14.
+//  Copyright (c) 2014 Mr Fridge Software & Digitale Akademie. All rights reserved.
+//
+
+#import "VAUAboutListViewController.h"
+
+@interface VAUAboutListViewController ()
+
+@end
+
+@implementation VAUAboutListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
