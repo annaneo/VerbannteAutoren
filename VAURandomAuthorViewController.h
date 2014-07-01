9c774adac4700eed1cae27c98a2fc187188825f9
annaneo <aneovesky@gmail.com>
2014-07-01 17:32:30 +0200

    - red tint color (not working in tap bar) - about text added - html parsing in aboutListViewController

---
+++ VAURandomAuthorViewController.h
@@ -0,0 +1,13 @@
+//
+//  VAURandomAuthorViewController.h
+//  VerbannteAutoren
+//
+//  Created by anna on 01.07.14.
+//  Copyright (c) 2014 Mr Fridge Software & Digitale Akademie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VAURandomAuthorViewController : UIViewController
+
+@end
