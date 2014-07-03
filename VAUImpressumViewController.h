746c18a857eaaff6fecec0aa399566959d3be7d7
annaneo <aneovesky@gmail.com>
2014-07-03 16:02:33 +0200

    Impressum.html  and ViewController added

---
+++ VAUImpressumViewController.h
@@ -0,0 +1,13 @@
+//
+//  VAUImpressumViewController.h
+//  VerbannteAutoren
+//
+//  Created by anna on 03.07.14.
+//  Copyright (c) 2014 Mr Fridge Software & Digitale Akademie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VAUImpressumViewController : UIViewController
+
+@end
